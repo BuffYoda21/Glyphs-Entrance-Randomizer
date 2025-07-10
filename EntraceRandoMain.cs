d2712ac7917f0cf490f1cf30e6f2a07ec3bc1172
Aaron Dill <aaronsacks2006@gmail.com>
2025-07-10 01:49:22 -0500

    fix: link Objectives to Requirements

--- EntraceRandoMain.cs
+++ EntraceRandoMain.cs
@@ -20,14 +20,13 @@ namespace GlyphsEntranceRando {
         public void LoadRandomizedEntrances() {
             string userDataDir = MelonLoader.Utils.MelonEnvironment.UserDataDirectory;
             string savePath = Path.Combine(userDataDir, "RandomizationResults.json");
-            bool loaded = false;
             if (File.Exists(savePath)) {
                 try {
                     string json = File.ReadAllText(savePath);
                     WarpManager.entrancePairs = JsonConvert.DeserializeObject<List<RoomShuffler.SerializedEntrancePair>>(json);
-                    if (WarpManager.entrancePairs != null && WarpManager.entrancePairs.Count == 54) {
+                    if (WarpManager.entrancePairs?.Count == 54) {
                         MelonLogger.Msg("Loaded existing RandomizationResults.json.");
-                        loaded = true;
+                        return; // stop here if we've loaded the entrances
                     } else {
                         MelonLogger.Warning("RandomizationResults.json is incomplete or invalid, generating new seed.");
                     }
@@ -35,14 +34,12 @@ namespace GlyphsEntranceRando {
                     MelonLogger.Error("Failed to load RandomizationResults.json, generating new seed.");
                 }
             }
-            if (!loaded) {
-                for (int i = 0; i > -1; i++) {
-                    if (RoomShuffler.Shuffle()) {
-                        MelonLogger.Msg($"{i} randomization attempts tried");
-                        MelonLogger.Msg("Randomization Successful!");
-                        LoadRandomizedEntrances();
-                        break;
-                    }
+            for (int i = 0; i > -1; i++) {
+                if (RoomShuffler.Shuffle()) {
+                    MelonLogger.Msg($"{i} randomization attempts tried");
+                    MelonLogger.Msg("Randomization Successful!");
+                    LoadRandomizedEntrances();
+                    break;
                 }
             }
         }
@@ -101,69 +98,69 @@ namespace GlyphsEntranceRando {
     }
 
     public enum Objective : byte {
-        None = 0x00,
+        None = Requirement.None,
 
         //Collectables and other counters
-        SilverShard = 0x01,
-        GoldShard = 0x02,
-        SmileToken = 0x05,
-        RuneCube = 0x0A,
-        VoidGateShard = 0x0B,
-        Sigil = 0x0C,
-        Glyphstone = 0x0D,
-        SerpentLock = 0x0F,
-        WallJump = 0x10,
-        Seeds = 0x12,    //placeholder need to check actual count
+        SilverShard = Requirement.SilverShardx15,
+        GoldShard = Requirement.GoldShardx1,
+        SmileToken = Requirement.SmileTokenx2,
+        RuneCube = Requirement.RuneCubex3,
+        VoidGateShard = Requirement.VoidGateShardx7,
+        Sigil = Requirement.Sigilx3,
+        Glyphstone = Requirement.Glyphstonex3,
+        SerpentLock = Requirement.SerpentLockx4,
+        WallJump = Requirement.WallJumpx1,
+        Seeds = Requirement.Seedsx10,    //placeholder need to check actual count
 
         //Abilities
-        Sword = 0x13,
-        DashOrb = 0x14,
-        Map = 0x15,
-        Grapple = 0x16,
-        DashAttackOrb = 0x17,
-        Parry = 0x18,
+        Sword = Requirement.Sword,
+        DashOrb = Requirement.DashOrb,
+        Map = Requirement.Map,
+        Grapple = Requirement.Grapple,
+        DashAttackOrb = Requirement.DashAttackOrb,
+        Parry = Requirement.Parry,
 
         //Story Points
-        ConstructDefeat = 0x19,
-        SerpentDefeat = 0x1A,
-        FalseEnding = 0x1B,
-        WizardTrueDefeat = 0x1C,
-        NullDefeat = 0x1D,
-        SpearmanDefeat = 0x1E,
-        GoodEnding = 0x1F,
-        Clarity = 0x20,
-        LastFracture = 0x21,
-        Act1 = 0x22,
-        Act2 = 0x23,
-        TrueEnding = 0x24,
-        SmilemaskEnding = 0x25,
-        OmnipotenceEnding = 0x26,
+        ConstructDefeat = Requirement.ConstructDefeat,
+        SerpentDefeat = Requirement.SerpentDefeat,
+        FalseEnding = Requirement.FalseEnding,
+        WizardTrueDefeat = Requirement.WizardTrueDefeat,
+        NullDefeat = Requirement.NullDefeat,
+        SpearmanDefeat = Requirement.SpearmanDefeat,
+        GoodEnding = Requirement.GoodEnding,
+        Clarity = Requirement.Clarity,
+        LastFracture = Requirement.LastFracture,
+        Act1 = Requirement.Act1,
+        Act2 = Requirement.Act2,
+        TrueEnding = Requirement.TrueEnding,
+        SmilemaskEnding = Requirement.SmilemaskEnding,
+        OmnipotenceEnding = Requirement.OmnipotenceEnding,
 
         //Shop Items
-        SwordRune = 0x27,
-        Shroud = 0x28,
-        FastMagic = 0x29,
-        SwiftParry = 0x2A,
+        SwordRune = Requirement.SwordRune,
+        Shroud = Requirement.Shroud,
+        FastMagic = Requirement.FastMagic,
+        SwiftParry = Requirement.SwiftParry,
 
         //Hats
-        PinkBow = 0x2B,
-        DummyHat = 0x2C,
-        TopHat = 0x2D,
-        TrafficCone = 0x2E,
-        JohnHat = 0x2F,
-        Fez = 0x30,
-        Hat7 = 0x31,
-        BombHat = 0x32,
-        Hat9 = 0x33,
-        George = 0x34,
+        PinkBow = Requirement.PinkBow,
+        DummyHat = Requirement.DummyHat,
+        TopHat = Requirement.TopHat,
+        TrafficCone = Requirement.TrafficCone,
+        JohnHat = Requirement.JohnHat,
+        Fez = Requirement.Fez,
+        Hat7 = Requirement.Hat7,
+        BombHat = Requirement.BombHat,
+        Hat9 = Requirement.Hat9,
+        George = Requirement.George,
 
         //Misc
-        FlowerPuzzle = 0x35,
-        VerticalMomentum = 0x36,
-        BreakableVoidPlatform = 0x37,
-        MapPuzzle = 0x38,
-        SaveButton = 0x39,
-        SaveShard = 0x3A,
+        FlowerPuzzle = Requirement.FlowerPuzzle,
+        VerticalMomentum = Requirement.VerticalMomentum,
+        BreakableVoidPlatform = Requirement.BreakableVoidPlatform,
+        MapPuzzle = Requirement.MapPuzzle,
+        SaveButton = Requirement.SaveButton,
+        SaveShard = Requirement.SaveShard,
     }
 
     public enum Requirement : byte {
