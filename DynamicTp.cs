ef06fc3569735bd0bd9b05f48b24bf4b0f8df5f1
BuffYoda21 <buffyoda21@gmail.com>
2025-07-11 16:04:55 -0500

    Added logic for "blocked" exits

--- DynamicTp.cs
+++ DynamicTp.cs
@@ -6,11 +6,20 @@ namespace GlyphsEntranceRando {
     public class DynamicTp : MonoBehaviour {
         public void OnTriggerEnter2D(Collider2D other) {
             if (other?.GetComponent<PlayerController>() == null || targetExit?.GetComponent<DynamicTp>() == null) return;
-            switch (type) {
-                case EntranceType.Left: other.transform.localPosition = new Vector3(targetExit.position.x - 1f, targetExit.position.y + ((other.transform.position.y - transform.position.y) / transform.localScale.y * targetExit.localScale.y), targetExit.position.z); break;
-                case EntranceType.Right: other.transform.localPosition = new Vector3(targetExit.position.x + 1f, targetExit.position.y + ((other.transform.position.y - transform.position.y) / transform.localScale.y * targetExit.localScale.y), targetExit.position.z); break;
-                case EntranceType.Top: other.transform.localPosition = new Vector3(targetExit.position.x + ((other.transform.position.x - transform.position.x) / transform.localScale.x * targetExit.localScale.x), targetExit.position.y + 1f, targetExit.position.z); break;
-                case EntranceType.Bottom: other.transform.localPosition = new Vector3(targetExit.position.x + ((other.transform.position.x - transform.position.x) / transform.localScale.x * targetExit.localScale.x), targetExit.position.y - 1f, targetExit.position.z); break;
+            if (!targetExit.GetComponent<DynamicTp>().blocked) {
+                switch (type) {
+                    case EntranceType.Left: other.transform.position = new Vector3(targetExit.position.x - 1f, targetExit.position.y + ((other.transform.position.y - transform.position.y) / transform.localScale.y * targetExit.localScale.y), targetExit.position.z); break;
+                    case EntranceType.Right: other.transform.position = new Vector3(targetExit.position.x + 1f, targetExit.position.y + ((other.transform.position.y - transform.position.y) / transform.localScale.y * targetExit.localScale.y), targetExit.position.z); break;
+                    case EntranceType.Top: other.transform.position = new Vector3(targetExit.position.x + ((other.transform.position.x - transform.position.x) / transform.localScale.x * targetExit.localScale.x), targetExit.position.y + 1f, targetExit.position.z); break;
+                    case EntranceType.Bottom: other.transform.position = new Vector3(targetExit.position.x + ((other.transform.position.x - transform.position.x) / transform.localScale.x * targetExit.localScale.x), targetExit.position.y - 1f, targetExit.position.z); break;
+                }
+            } else {
+                switch (type) {
+                    case EntranceType.Left: other.transform.position = new Vector3(other.transform.position.x + 1f, other.transform.position.y, other.transform.position.z); break;
+                    case EntranceType.Right: other.transform.position = new Vector3(other.transform.position.x - 1f, other.transform.position.y, other.transform.position.z); break;
+                    case EntranceType.Top: other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y - 1f, other.transform.position.z); break;
+                    case EntranceType.Bottom: other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 1f, other.transform.position.z); break;
+                }
             }
         }
         public void RegisterTargetFromId(List<GameObject> warps) {
@@ -22,5 +31,6 @@ namespace GlyphsEntranceRando {
         public int id;
         public int targetId;
         public EntranceType type;
+        public bool blocked = false;
     }
 }
---
+++ WorldManager.cs
@@ -0,0 +1,63 @@
+using System;
+using Il2Cpp;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GlyphsEntranceRando {
+    public class WorldManager : MonoBehaviour {
+        public void Update() {
+            if (SceneManager.GetActiveScene().name != "Game") return;
+            if (!warpParent)
+                warpParent = GameObject.Find("Warps")?.transform;
+            if (!warpParent) return;
+            if (!constructDoorEnter || !constructDoorExit || !r1FlowerEnter || !r1FlowerExit) {
+                constructDoorEnter = warpParent.Find($"0x{CONSTRUCT_DOOR_ENTER:X4}")?.GetComponent<DynamicTp>();
+                constructDoorExit = warpParent.Find($"0x{CONSTRUCT_DOOR_EXIT:X4}")?.GetComponent<DynamicTp>();
+                r1FlowerExit = warpParent.Find($"0x{R1_FLOWER_EXIT:X4}")?.GetComponent<DynamicTp>();
+                r1FlowerEnter = warpParent.Find($"0x{R1_FLOWER_ENTER:X4}")?.GetComponent<DynamicTp>();
+                if (!constructDoorEnter || !constructDoorExit || !r1FlowerEnter || !r1FlowerExit) return;
+            }
+            if (!player)
+                player = GameObject.Find("Player")?.transform;
+
+            //temporary solution most likely
+            if (player && Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Backspace)
+                player.position = new Vector3(0f, 0f, 0f);
+
+            if (!runicConstruct) {
+                if (!constructParent)
+                    constructParent = GameObject.Find("World/Region1/Runic Construct(R3E)")?.transform;
+                if (constructParent)
+                    runicConstruct = constructParent.Find("DashBoss")?.gameObject;
+                if (runicConstruct) {
+                    constructDoorEnter.blocked = true;
+                    constructDoorExit.blocked = true;
+                } else {
+                    constructDoorEnter.blocked = false;
+                    constructDoorExit.blocked = false;
+                }
+            }
+
+            // More logic coming later once flower puzzle is implemented. For now these should always be blocked.
+            if (!r1FlowerEnter.blocked)
+                r1FlowerEnter.blocked = true;
+            if (!r1FlowerExit.blocked)
+                r1FlowerExit.blocked = true;
+        }
+
+        Transform player;
+        Transform warpParent;
+        Transform constructParent;
+        GameObject runicConstruct;
+        DynamicTp constructDoorExit;
+        DynamicTp constructDoorEnter;
+        DynamicTp r1FlowerExit;
+        DynamicTp r1FlowerEnter;
+
+        //these entrances may be blocked under certain conditions
+        public const int CONSTRUCT_DOOR_EXIT = 0x0033;
+        public const int CONSTRUCT_DOOR_ENTER = 0x0032;
+        public const int R1_FLOWER_EXIT = 0x0016;
+        public const int R1_FLOWER_ENTER = 0x0028;
+    }
+}
